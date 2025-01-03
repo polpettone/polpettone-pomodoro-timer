a82587b74b03afb47eab14cfa36e642e33325fbe
Stefan Heiden <stefanheiden@gmail.com>
2025-01-03 18:05:14 +0100

    initial

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "pomodoro")]
+struct Cli {
+    /// Duration in minutes
+    #[structopt(short = "t", long = "duration", default_value = "25")]
+    duration: i32,
+
+    /// Description of this session
+    #[structopt(short = "d", long = "description", default_value = "no description")]
+    description: String,
+
+    /// Command to execute when session finished
+    #[structopt(short = "f", long = "finishCommand", default_value = "i3lock")]
+    finish_command: String,
+}
+
+fn main() {
+    let args = Cli::from_args();
+    println!("Duration: {} minutes", args.duration);
+    println!("Description: {}", args.description);
+    println!("Finish Command: {}", args.finish_command);
+}

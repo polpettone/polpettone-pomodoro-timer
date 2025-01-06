63d331807196d40bbbcd4cd32740cadfd0aa4b9c
Stefan Heiden <stefanheiden@gmail.com>
2025-01-06 16:17:04 +0100

    session struct

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,34 @@ struct Cli {
     finish_command: String,
 }
 
+use chrono::{serde::ts_seconds, DateTime, NaiveDateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
+
+// Defining the Session struct in Rust
+#[derive(Serialize, Deserialize, Debug)]
+struct Session {
+    description: String,
+    duration: Duration,
+    #[serde(with = "ts_seconds")]
+    start: DateTime<Utc>,
+}
+
 fn main() {
     let args = Cli::from_args();
     println!("Duration: {} minutes", args.duration);
     println!("Description: {}", args.description);
     println!("Finish Command: {}", args.finish_command);
+
+    let naive_datetime =
+        NaiveDateTime::parse_from_str("2023-10-10 10:00:00", "%Y-%m-%d %H:%M:%S").unwrap();
+    let start: DateTime<Utc> = DateTime::from_naive_utc_and_offset(naive_datetime, Utc);
+
+    let test_session = Session {
+        description: String::from("Example session"),
+        duration: Duration::new(3600, 0), // 1 hour in seconds
+        start,
+    };
+
+    println!("{:?}", test_session);
 }

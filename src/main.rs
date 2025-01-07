411525b3664553385115fe741dd073769419fece
Stefan Heiden <stefanheiden@gmail.com>
2025-01-07 15:08:41 +0100

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,14 @@
 mod session;
+mod view;
 
 use crate::session::SessionService;
+use crate::view::Counter;
+
 use serde::Deserialize;
 use structopt::StructOpt;
 
 use std::error::Error;
 use std::fs;
-
-use iced::widget::{button, column, text, Column};
-
-impl Counter {
-    pub fn view(&self) -> Column<Message> {
-        // We use a column: a simple vertical layout
-        column![
-            // The increment button. We tell it to produce an
-            // `Increment` message when pressed
-            button("+").on_press(Message::Increment),
-            // We show the value of the counter here
-            text(self.value).size(50),
-            // The decrement button. We tell it to produce a
-            // `Decrement` message when pressed
-            button("-").on_press(Message::Decrement),
-        ]
-    }
-    pub fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
-    }
-}
-
-#[derive(Default)]
-struct Counter {
-    value: i32,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
-    Increment,
-    Decrement,
-}
 
 #[derive(Debug, Deserialize)]
 pub struct PomodoroConfig {
